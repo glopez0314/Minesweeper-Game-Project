e30f58d93a389cb82069e318f4d128610cc2492d
glopez0314 <garyjlopez3.14@gmail.com>
2023-06-27 10:47:34 -0700

    add input and game over function

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 //constants//
-const PLAYER = "player";
+
 
 //state variables//
-let player;
 let board;
-
+let mineAmount = 10;
+let minePosition = [];
 
 //cached varriables//
 const playAgn = document.getElementById("play-agn");
 const boardEl = document.querySelectorAll(".cell");
 const flagBtn = document.getElementById('flag-btn');
-const mines = document.getElementById('mineLeft');
+const mineCount = document.getElementById('minesLeft');
 
 //EventListener//
 playAgn.addEventListener("click", init);
@@ -27,9 +27,10 @@ function init() {
     boardEl.forEach(cell => {
         cell.classList.remove('clicked');
         cell.innerHTML = "";
+        cell.style.backgroundColor = '';
     });
     flagBtn.classList.replace('toggled', 'not-toggled');
-
+    minePosition = [];
     board = [
         0, 0, 0, 0, 0, 0, 0, 0,
         0, 0, 0, 0, 0, 0, 0, 0,
@@ -39,13 +40,13 @@ function init() {
         0, 0, 0, 0, 0, 0, 0, 0,
         0, 0, 0, 0, 0, 0, 0, 0,
         0, 0, 0, 0, 0, 0, 0, 0,
-    ];
+    ]
     render();
 }
 
 /* have a function that renders the board*/
 function render(){
-    renderBoard();
+    renderMines();
     renderMessage();
 }
 
@@ -55,23 +56,33 @@ function render(){
 function handleInput(evt) {
     const cell = evt.target;
     if (flagBtn.classList == 'toggled') {
-        cell.innerHTML = '🚩';
+        if (cell.innerHTML == '') {
+            cell.innerHTML = '🚩';
+        } else {
+            cell.innerHTML = '';
+        };
+    }else if (minePosition.includes(cell.id)) {
+        return gameOver();
     } else {
-        evt.target.classList.add('clicked');
+        cell.classList.add('clicked');
+        countAdjMines();
     }
 }
 
 /* Have an end game function that activates if the player has passed the level or stepped/"clicked" on a mine */
-// function gameOver() {
-//     if {
-
-//     } else {
-
-//     }
-// }
-
+ function gameOver() {
+    boardEl.forEach((cell) => {
+        if (minePosition.includes(cell.id)) {
+            cell.innerHTML = '💣';
+            cell.style.backgroundColor = 'red';
+        }
+    })
+ }
 /* have a function to count adjacent mines an tag the squares next to the mines */
 function countAdjMines() {
+    let minesFound = 0;
+    minesFound += countAdjacent();
+    minesFound += countAdjacent();
 
 }
 
@@ -84,12 +95,20 @@ function toggle() {
     this.classList.toggle('toggled');
 }
 
-function renderBoard() {
-    board.forEach((boxval, idx) => {
-        
-    })
+function renderMines() {
+    let minesGenerated = mineAmount;
+    while(minesGenerated > 0) {
+        let row = Math.floor(Math.random() * 8);
+        let column = Math.floor(Math.random() * 8);
+        let idx = row.toString() + "-" + column.toString();
+        if (!minePosition.includes(idx)) {
+            minePosition.push(idx);
+            minesGenerated -= 1;
+        }
+    }
 }
 
 function renderMessage() {
-    
+    mineCount.innerHTML = mineAmount;
+
 }
