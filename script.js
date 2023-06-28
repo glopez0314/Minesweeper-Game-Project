84afd553250ac56dcf69559af37f71f5ab4057ba
glopez0314 <garyjlopez3.14@gmail.com>
2023-06-28 15:15:06 -0700

    add recursion and checkWin

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 //constants//
 
-
 //state variables//
-let board;
 let mineAmount = 10;
 let minePosition = [];
-let endGame = false;
+let marked = 0;
+let cellsClicked = 0;
 
 //cached varriables//
 const playAgn = document.getElementById("play-agn");
 const boardEl = document.querySelectorAll(".cell");
 const flagBtn = document.getElementById('flag-btn');
+const barOne = document.getElementById('info');
+const barTwo = document.getElementById('time');
+const barThree = document.getElementById('bar-three');
+const barFour = document.getElementById('bar-four');
 const mineCount = document.getElementById('minesLeft');
 
 //EventListener//
@@ -27,21 +30,29 @@ init();
 function init() {
     boardEl.forEach(cell => {
         cell.classList.remove('clicked');
+        cell.classList.remove('m-1');
+        cell.classList.remove('m-2');
+        cell.classList.remove('m-3');
+        cell.classList.remove('m-4');
+        cell.classList.remove('m-5');
+        cell.classList.remove('m-6');
+        cell.classList.remove('m-7');
+        cell.classList.remove('m-8');
         cell.innerHTML = "";
         cell.style.backgroundColor = '';
     });
+    flagBtn.classList.remove('hide');
+    playAgn.classList.remove('show');
+    barOne.classList.remove('hide');
+    barTwo.classList.remove('hide');
+    barThree.classList.remove('show');
+    barFour.classList.remove('show');
     flagBtn.classList.replace('toggled', 'not-toggled');
+    cellsClicked = 0;
     minePosition = [];
-    board = [
-        0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0,
-    ]
+    mineAmount = 10;
+    marked = 0;
+    endGame = false;
     render();
 }
 
@@ -56,25 +67,50 @@ function render(){
 /* have a players move function and event listener */
 function handleInput(evt) {
     const cell = evt.target;
-    if (cell.classList.contains("click") || endGame) {
+    if (cell.classList.contains("clicked") || endGame) {
         return;
     } else if (flagBtn.classList == 'toggled') {
         if (cell.innerHTML == '') {
             cell.innerHTML = '🚩';
+            marked += 1;
+            renderMessage();
         } else {
             cell.innerHTML = '';
+            marked -= 1;
+            renderMessage();
         };
     }else if (minePosition.includes(cell.id)) {
         return gameOver();
     } else {
-        cell.classList.add('clicked');
-        countAdjMines();
+        if (cell.innerHTML == '🚩') {
+        cell.innerHTML = '';
+        marked -= 1;
+        renderMessage();
+        }
+        const num = cell.id.split("-");
+        let row = parseInt(num[0]);
+        let col = parseInt(num[1]);
+        countAdjMines(row, col);
+        checkWin();
     }
 }   
 
-/* Have an end game function that activates if the player has passed the level or stepped/"clicked" on a mine */
- function gameOver() {
+function checkWin() {
+    if (cellsClicked === 54) {
+        endGame = true;
+        flagBtn.classList.add('hide');
+        playAgn.classList.add('show');
+    }
+} 
+
+function gameOver() {
     endGame = true;
+    flagBtn.classList.add('hide');
+    playAgn.classList.add('show');
+    barOne.classList.add('hide');
+    barTwo.classList.add('hide');
+    barThree.classList.add('show');
+    barFour.classList.add('show');
     boardEl.forEach((cell) => {
         if (minePosition.includes(cell.id)) {
             cell.innerHTML = '💣';
@@ -83,12 +119,53 @@ function handleInput(evt) {
     })
  }
 /* have a function to count adjacent mines an tag the squares next to the mines */
-function countAdjMines(row, column) {
-    //if ()
-    let minesFound = 0;
+function countAdjMines(row, col) {
+    const cell = document.getElementById(row.toString() + "-" + col.toString())
+    if (row < 0 || row >= 8 || col < 0 || col >= 8) {
+        return;
+    }
+    if (cell.classList.contains('clicked')) {
+        return;
+    }
+    cell.classList.add('clicked');
+    cellsClicked += 1;
+    let minesNearby = 0;
    
+    minesNearby += checkAdjacent(row - 1, col - 1);
+    minesNearby += checkAdjacent(row - 1, col);
+    minesNearby += checkAdjacent(row - 1, col + 1);
+    minesNearby += checkAdjacent(row, col + 1);
+    minesNearby += checkAdjacent(row + 1, col + 1);
+    minesNearby += checkAdjacent(row + 1, col);
+    minesNearby += checkAdjacent(row + 1, col - 1);
+    minesNearby += checkAdjacent(row, col - 1);
+    if (minesNearby > 0) {
+        cell.innerText = minesNearby;
+        cell.classList.add(`m-${minesNearby}`);
+    } else {
+        countAdjMines(row - 1, col - 1,);
+        countAdjMines(row - 1, col,);
+        countAdjMines(row - 1, col + 1,);
+        countAdjMines(row, col + 1,);
+        countAdjMines(row + 1, col + 1,);
+        countAdjMines(row + 1, col,);
+        countAdjMines(row + 1, col - 1,);
+        countAdjMines(row, col - 1,);
+    }
+    
 
+    function checkAdjacent(row, col) {
+        if (row < 0 || row >= 8 || col < 0 || col >= 8) {
+            return 0;
+        }
+        if (minePosition.includes(row.toString() + "-" + col.toString())) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
 }
+
 
 /* have a function that will open adjacent empty spaces not touching a mine */
 //flood function?
@@ -113,5 +190,5 @@ function renderMines() {
 }
 
 function renderMessage() {
-    mineCount.innerHTML = mineAmount;
-}
+    mineCount.innerHTML = (mineAmount - marked);
+};
